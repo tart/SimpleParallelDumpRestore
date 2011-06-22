6bd8c826e441e35cec8e77d9fa3a4ef62d6cbd56
Emre Hasegeli <hasegeli@tart.com.tr>
2011-06-22 15:08:10 +0300

    Notifications added.

--- dumpDatabase.sh
+++ dumpDatabase.sh
@@ -7,8 +7,13 @@
  # @date        2011-05-23
  ##
 
-mysqldump --no-data --routines $1 > $1".schema.sql" 2> $1".dump.log"
+echo "Dumping schema to \""$1".schema.sql\"..." >> $1".dump.log"
+echo "Dumping schema to \""$1".schema.sql\"..."
+( time mysqldump --no-data --routines $1 > $1".schema.sql" 2>> $1".dump.log" ) 2>> $1".dump.log"
+echo >> $1".dump.log"
 
+echo "Dumping data to \""$1".data\"..." >> $1".dump.log"
+echo "Dumping data to \""$1".data\"..."
 mkdir $1".data"
 chmod o+w $1".data"
 
@@ -17,7 +22,10 @@ tables=$(mysql --execute "Show full tables where Table_type = 'BASE TABLE';" $1 
 		sed "s/\tBASE TABLE//;")
 for table in $tables
 	do
-		mysql --execute "Select SQL_NO_CACHE * from "$table" into outFile '"$(pwd)"/"$1".data/"$table"'" $1 2> $1".dump.log" &
+		mysql --execute "Select SQL_NO_CACHE * from "$table" into outFile '"$(pwd)"/"$1".data/"$table"'" $1 2>> $1".dump.log" &
 	done
 
+echo "Waiting..."
+( time wait ) 2>> $1".dump.log"
+echo >> $1".dump.log"
 exit 0
--- restoreDatabase.sh
+++ restoreDatabase.sh
@@ -7,15 +7,23 @@
  # @date        2011-05-23
  ##
 
-mysql $2 < $1".schema.sql" 2>> $1".restore.log"
+echo "Restoring schema from \""$1".schema.sql\" to "$2" database..." >> $1".restore.log"
+echo "Restoring schema from \""$1".schema.sql\" to "$2" database..."
+( time mysql $2 < $1".schema.sql" 2>> $1".restore.log" ) 2>> $1".restore.log"
+echo >> $1".restore.log"
 
+echo "Restoring data from \""$1".data\" to "$2" database..." >> $1".restore.log"
+echo "Restoring data from \""$1".data\" to "$2" database..."
 cd $1".data"
 
 for table in *
 	do
 		if [ -s $table ]; then
-			mysql --execute "Load data inFile '"$(pwd)"/"$table"' into table "$table"; $2 2>> $1".restore.log" &
+			mysql --execute "Load data inFile '"$(pwd)"/"$table"' into table "$table $2 2>> $1".restore.log" &
 		fi
 	done
 
+echo "Waiting..."
+( time wait ) 2>> $1".restore.log"
+echo >> $1".restore.log"
 exit 0
