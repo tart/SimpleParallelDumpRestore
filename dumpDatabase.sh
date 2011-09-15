9d8715877ca48bf4efaef71ed80cf64cddd60879
Emre Hasegeli <hasegeli@tart.com.tr>
2011-09-15 10:49:04 +0300

    Slave checked for running.

--- dumpDatabase.sh
+++ dumpDatabase.sh
@@ -7,14 +7,25 @@
  # @date        2011-05-23
  ##
 
+if [ -e slaveStatus ]
+    then
+        echo "File matching \"slaveStatus\" exists." > /dev/stderr
+        exit 1
+    fi
+
+slaveRunning=$(mysql -e "Show status like 'Slave_running'" | sed 1d | cut -f 2)
+
+if [ $slaveRunning = "ON" ]
+    then
+        echo "Stopping slave activity..."
+        mysql -e "Stop slave"
+    fi
+
 if [ -e $1"."* ]
 	then
 		echo "File matching \""$1".*\" exists." > /dev/stderr
 		exit 1
 	fi
-
-echo "Stopping slave activity..."
-mysql -e "Stop slave"
 
 echo "Dumping data model to \""$1".dataModel.sql\"..."
 mysqldump -dR $1 > $1".dataModel.sql"
@@ -32,17 +43,17 @@ for table in $(mysql -e "Show full tables where Table_type = 'BASE TABLE'" $1 | 
 echo "Dumping master status..."
 mysql -e "Show master status" > masterStatus
 
-echo "Dumping slave status..."
-mysql -e "Show slave status" > slaveStatus
-
 if [ -s masterStatus ]
 	then
 		echo "Flushing logs..."
 		mysql -e "Flush logs"
 	fi
 
-if [ -s slaveStatus ]
+if [ $slaveRunning = "ON" ]
 	then
+        echo "Dumping slave status..."
+        mysql -e "Show slave status" > slaveStatus
+
 		echo "Starting slave activity..."
 		mysql -e "Start slave"
 	fi
