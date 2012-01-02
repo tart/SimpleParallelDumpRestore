2adc63b6b24f6e1c83eb91e768ffa528a9675022
Emre Hasegeli <hasegeli@tart.com.tr>
2012-01-02 19:10:57 +0200

    "-j" parameter added to the dumpDatabase script using GNU parallel tool.

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 ## Usage:
 
-./dumpDatabase.sh
+./dumpDatabase.sh [-j jobs]
 
 ./restoreSchema.sh \<oldSchemaName\> \<newSchemaName\>
 
 ## Description
 
-Dumps and restores all tables parallel in a MySQL database.
+Dumps and restores all tables in a MySQL database parallel using GNU parallel
+tool [1].
 
-Dump command creates a schema file and one data file for all
-tables using "Select ... into outFile" statement with "mysqldump"
-command. Restore command executes this files using "Load data
-inFile" statement.
+Dump command creates a schema file and one data file for all tables using
+"Select ... into outFile" statement with "mysqldump" command. Restore command
+executes this files using "Load data inFile" statement.
 
-MySQL user with sufficient privileges should be specified in
-"my.cnf".
+MySQL user with sufficient privileges should be specified in "my.cnf".
+
+[1] http://www.gnu.org/software/parallel
--- dumpDatabase.sh
+++ dumpDatabase.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
  ##
  # Tart Database Operations
  # Simple MySQL Parallel Dump & Restore
@@ -6,6 +6,25 @@
  # @author      Emre Hasegeli <hasegeli@tart.com.tr>
  # @date        2011-05-23
  ##
+
+#
+# Fetching the parameters
+#
+
+while getopts "j:" opt; do
+	case $opt in
+		j )	jobs="-j $OPTARG " ;;
+		h )	echo "a script to dump MySQL database parallel"
+			echo "Usage:"
+			echo "$0 -h"
+			echo "$0 [-j jobs]"
+			echo "Source:"
+			echo "github.com/tart/SimpleMySQLParallelDumpRestore"
+			exit 1 ;;
+		\? )	echo "Wrong parameter." > /dev/stderr
+			exit 1
+	esac
+done
 
 if [ -e slaveStatus ]; then
     echo "File matching \"slaveStatus\" exists." > /dev/stderr
@@ -19,17 +38,9 @@ if [ -e masterStatus ]; then
     exit 1
 fi
 
-if [ -e *.dataModel.sql ]; then
-    echo "File matching \"*.dataModel.sql\" exists." > /dev/stderr
-
-    exit 1
-fi
-
-if [ -e *.data ]; then
-    echo "File matching \"*.data\" exists." > /dev/stderr
-
-    exit 1
-fi
+#
+# Preparing the database server
+#
 
 slaveRunning=$(mysql -e "Show status like 'Slave_running'" | sed 1d | cut -f 2)
 if [ $slaveRunning = "ON" ]; then
@@ -40,19 +51,25 @@ fi
 echo "Locking tables..."
 mysql -e "Flush tables with read lock"
 
-for schema in $(mysql -e "Show schemas" | sed 1d); do
+#
+# Dumping
+#
+
+for schema in $(mysql -e "Show schemas" | sed 1d | sed /_schema$/d); do
+    tables=$(mysql -e "Show full tables where Table_type = 'BASE TABLE'" $schema | sed 1d | cut -f 1)
+
     echo "Dumping data model of $schema..."
-    mysqldump -dR --skip-lock-tables $schema > $schema.dataModel.sql
+    mysqldump -dR $schema > $schema.dataModel.sql
 
     echo "Dumping data of $schema..."
     mkdir $schema.data
     chmod o+w $schema.data
-    for table in $(mysql -e "Show full tables where Table_type = 'BASE TABLE'" $schema | sed 1d | cut -f 1); do
-        mysql -e "Set sql_big_selects = 1;
-                Set query_cache_type = 0;
-                Select * from $schema.$table into outfile '"$(pwd)/$schema".data"/$table"'"  &
-    done
+    parallel $jobs"mysql -e \"Select * from {} into outfile '\"$(pwd)/$schema.data/{}\"'\" $schema" ::: $tables
 done
+
+#
+# Preparing the database server
+#
 
 echo "Unlocking tables..."
 mysql -e "Unlock tables"
