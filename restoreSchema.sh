bd2ad99ba20bf449d149e11fd3598fe7bac03f0c
Emre Hasegeli <emre.hasegeli@tart.com.tr>
2012-03-13 19:19:59 +0200

    "-j" parameter added to the restoreSchema script using GNU parallel tool.

--- restoreSchema.sh
+++ restoreSchema.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
  ##
  # Tart Database Operations
  # Simple MySQL Parallel Dump & Restore
@@ -7,30 +7,47 @@
  # @date        2011-05-23
  ##
 
-if [ ! -f $1".datamodel.sql" ]; then
-    echo "File matching \""$1".datamodel.sql\" does not exists." > /dev/stderr
+#
+# Fetching the parameters
+#
+
+while getopts "j:" opt; do
+	case $opt in
+		j )	jobs=$OPTARG ;;
+		h )	echo "a script to restore MySQL schema parallel"
+			echo "Usage:"
+			echo "$0 -h"
+			echo "$0 [-j jobs] <oldSchemaName> <newSchemaName>"
+			echo "Source:"
+			echo "github.com/tart/SimpleMySQLParallelDumpRestore"
+			exit 1 ;;
+		\? )	echo "Wrong parameter." > /dev/stderr
+			exit 1
+	esac
+done
+
+if [ ! -f $1.dataModel.sql ]; then
+    echo "File matching \"$1.dataModel.sql\" does not exists." > /dev/stderr
 
     exit 1
 fi
 
-if [ ! -d $1".data" ]; then
-    echo "Directory matching \""$1".data\" does not exists." > /dev/stderr
+if [ ! -d $1.data ]; then
+    echo "Directory matching \"$1.data\" does not exists." > /dev/stderr
 
     exit 1
 fi
+
+#
+# Restoring
+#
 
 echo "Restoring data model from \""$1".dataModel.sql\" to "$2" database..."
 mysql $2 < $1".dataModel.sql"
 
 echo "Restoring data from \""$1".data\" to "$2" database..."
-for table in $1".data"/*; do
-    if [ -s $table ]; then
-        mysql -e "Set unique_checks = 0;
-                Set foreign_key_checks = 0;
-                Load data infile '"$(pwd)/$table"' into table "${table#$1".data/"} $2 &
-    fi
-done
+parallel $jobs"mysql -e \"Set unique_checks = 0; \
+        Set foreign_key_checks = 0; \
+        Load data infile '$(pwd)/{}' into table {/} character set 'utf8' $2" ::: $(ls $1.data/*)
 
-wait
 exit 0
-
